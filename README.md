dbfb620c7f0985803230bd655207daac7d1f8470
Samuli Piipponen <piipponen.samuli@gmail.com>
2019-05-14 09:52:58 +0300

    Moved project from other gitlab

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# FreeGamesBot
+### You need to add your own config.json that needs to have:
 
-Discord bot that seeks for free games and notifies about them.
+
+```json
+{
+  "token": "insert bot token",
+  "version": "1.0.0",
+  "updateDate": "13.05.2019",
+  "embedColor": "6648024",
+  "channel_id": "insert channel id"
+}
+```
+
+### where: 
+- token is the bot's token that you get when creating the bot to API
+- embedcolor is the  bordercolor of the message on discord
+- channel_id is the id of channel you wish the messages to be sent to
+
+## Planning to add:
+- Saving 5-10 latest post to memory in order to prevent dublicates
+- more sources to find free gmae notifications from
+- timeouts
+- better filtering
---
+++ bot.js
@@ -0,0 +1,118 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+const client = new Discord.Client();
+const filters = require("./filters.json");
+const request = require("request");
+
+let date = getTime();
+let selector = 0;
+
+// Do every hour
+setInterval(function GameDealsNew(){
+	freeGames(date);
+	date = getTime();
+}, 3600000);
+
+function getTime(){
+	let dateWhole = new Date();
+	let date = dateWhole.getDate() + "." + (dateWhole.getMonth() + 1) + " " + dateWhole.getHours() + ":" + dateWhole.getMinutes();
+	return date;
+}
+
+function freeGames(){
+	request({
+  		url: 'https://www.reddit.com/r/gamedeals/new.json',
+ 		json: true
+	}, function(error, response, list) {
+		let selector = 0;
+		chooseGame(list);
+	});
+}
+
+function chooseGame(list){
+	let game = list.data.children[selector].data;
+	selector++;
+	game = list.data.children[selector].data
+	console.log("\nCurrent game: " + game.title);
+
+	let unixTime = (new Date).getTime() / 1000; //check if the post is older than an hour
+	if(game.created_utc > (unixTime - 3800)){
+		filterGame(game);
+		chooseGame(list);
+	}
+	else{
+		console.log("Posted over an hour ago");
+		selector = 0;
+	}
+}
+
+function filterGame(game){
+	console.log("filtering...");
+	let valid = 0;
+	let i = 0;
+	filters.whitelist.forEach(function(){
+		if (game.title.toLowerCase().includes(filters.whitelist[i])){
+			valid = 1;
+			console.log(valid);
+		}
+		i++;
+	});
+	i = 0;
+	filters.blacklist.forEach(function(){
+		if (game.title.toLowerCase().includes(filters.blacklist[i])){
+			valid = 0;
+		}
+		i++;
+	});
+	let wrongPercentOff = game.title.match(/\d{2,3}%/ig); //check the other precentages for 'free' in wrong context
+	if(wrongPercentOff != null){
+		let wrongPercentNumber = wrongPercentOff[0].match(/\d{2,3}/g);
+		if(wrongPercentNumber < 100){
+			valid = 0;
+		}
+	}
+	let wrongMoneyOff = game.title.match(/\d{2,4}€|€\d{2,3}|\$\d{2,3}|£\d{2,3}/ig);
+	if(wrongMoneyOff != null){
+		let wrongMoneyNumber = wrongMoneyOff[0].match(/\d{2,3}/g);
+		if(wrongMoneyNumber > 0){
+			valid = 0;
+		}
+	}
+	if(valid == 1){ 
+		sendGame(game.title, game.url, game.thumbnail); 
+		console.log("free");
+	}
+	else console.log("not free");
+}
+
+function sendGame(gameTitle, gameUrl, gameThumb){
+	if(gameThumb == "default"){
+		gameThumb = "https://puu.sh/B8rUY.jpg";
+	}
+	embed = {
+		"title": "Free Game!",
+		"description": gameTitle + ": " + gameUrl,
+		"color": config.embedColor,
+		"image": {
+			"url": gameThumb
+		},
+		"thumbnail": {
+			"url": "https://puu.sh/AZxe5.png"
+		}
+	};
+	client.channels.get(config.channel_id).send({ embed });
+}
+
+console.log(date + ": Setup Done!");
+
+client.on("ready", function() {
+	client.user.setActivity("for free games!", { type: "WATCHING" });
+	console.log(date + ": Connected!");
+	console.log("Version: " + config.version + " - Last Updated: " + config.updateDate);
+	freeGames();
+});
+
+client.on('uncaughtException', (e) => console.error(date + ": " + e));
+client.on('error', (e) => console.error(date + ": " + e));
+client.on('warn', (e) => console.error(date + ": " + e));
+client.login(config.token);
